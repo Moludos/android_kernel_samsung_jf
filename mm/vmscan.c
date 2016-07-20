aa752a254b43971469bc940bdc0b0d65621fffcb
Joe Maples <joe@frap129.org>
2016-07-20 16:06:43 +0000

    mm: Set vm_swappiness to 0
    Swap is disabled, so theres no reason to want to place memory into swap
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Conflicts:
    	mm/vmscan.c

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -146,8 +146,18 @@ struct mem_cgroup_zone {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
-long vm_total_pages;	/* The total number of pages which the VM controls */
+int vm_swappiness = 0;
+/*
+ * The total number of pages which are beyond the high watermark within all
+ * zones.
+ */
+unsigned long vm_total_pages;
+
+#ifdef CONFIG_KSWAPD_CPU_AFFINITY_MASK
+char *kswapd_cpu_mask = CONFIG_KSWAPD_CPU_AFFINITY_MASK;
+#else
+char *kswapd_cpu_mask = NULL;
+#endif
 
 #ifdef CONFIG_RUNTIME_COMPCACHE
 extern int get_rtcc_status(void);
